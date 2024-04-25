1653b7633246f8b3b151cbe08f09a482ce9cb0ec
Telmo Checa <telmocheca1@gmail.com>
2024-04-25 23:39:14 +0200

    Primo commit

---
+++ src/org/lessons/java/geometria/Main.java
@@ -0,0 +1,18 @@
+package org.lessons.java.geometria;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Ciao Geometria");
+		
+		//inpoto lo scanner
+		Scanner input = new Scanner(System.in);
+		
+		//chiedo al utente il valore della base
+		System.out.println("Inserisce il valore della base del rettangolo!");
+
+	}
+
+}
