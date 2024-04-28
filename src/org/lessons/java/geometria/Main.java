9094a0b3dc73fea5db4bb5ac546ed168b3dffc38
Telmo Checa <telmocheca1@gmail.com>
2024-04-28 18:53:05 +0200

    ADD: classi Disegno.java, Main.java, Rettangolo.java

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -4,14 +4,46 @@ import java.util.Scanner;
 
 public class Main {
 
+
 	public static void main(String[] args) {
 		System.out.println("Ciao Geometria");
 		
-		//inpoto lo scanner
+		//richiamo class Rettangolo per costruire il rettangolo
+		//si richima al classe con il nome della variabile datta
+		Rettangolo rettangolo = new Rettangolo();
+		
+		//importo il metodo scanner
 		Scanner input = new Scanner(System.in);
 		
-		//chiedo al utente il valore della base
-		System.out.println("Inserisce il valore della base del rettangolo!");
+		//chiedo al utente la misura della base
+		System.out.println("Inserisci la misura della base del rettangolo");
+		
+		//assegno il valore delinpit alla variabile del oggeto "retangolo.base"
+		rettangolo.base = input.nextInt();
+//		System.out.println(rettangolo.base);
+		
+		//chiedo al utente la misura dell'altezza
+		System.out.println("Inserisci la misura dell'altezza  del rettangolo");
+		
+		//assegno il valore delinpit alla variabile del oggeto "retangolo.altezza"
+		rettangolo.altezza = input.nextInt();
+//		System.out.println(rettangolo.altezza);
+		
+		//variabili principali
+		int area;
+		int perimetro;
+		
+		//invoco i metodi della classe Rettangolo per i calcoli e assegno alla variabile corrispondente
+		area = rettangolo.calcoloArea();
+		System.out.println(area);
+		
+		//invoco i metodi della classe Rettangolo per i calcoli e assegno alla variabile corrispondente
+		perimetro = rettangolo.calcoloPerimetro();
+		System.out.println(perimetro);
+		
+//		*********DISEGNO RETTANGOLO**************
+		
+		//invoco il metodo che stampa il disegno del rettangolo
 
 	}
 
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,5 +1,22 @@
 package org.lessons.java.geometria;
 
 public class Rettangolo {
+	
+	// creo le variabili (istanzia)
+	int base;
+	int altezza;
+	
+	//calcolo del area (metodo)
+	int calcoloArea() {
+		int area = base * altezza;
+		return area;
+	}
+	
+	//calcolo ilperimetro (metodo)
+	int calcoloPerimetro() {
+		int perimetro = (base + altezza) * 2;
+		return perimetro;
+	}
+	
 
 }
