f9d83481e792511d11b9e78b2363ce960c355493
Telmo Checa <telmocheca1@gmail.com>
2024-05-09 13:17:27 +0200

    agiornamento disegno

--- src/org/leson/java/geometria/oop/DisegnoFigura.java
+++ src/org/leson/java/geometria/oop/DisegnoFigura.java
@@ -10,20 +10,20 @@ public class DisegnoFigura {
 		//uso come nome della variabile x nel for che corrisponde al asse orizontale == base
 		
 			//il for del asse y itera il ciclo per disegnare ogni linea del rettangolo dal alto verso il basso
-			for(int  y = 0;  y <= altezza; y++) {
+			for(int  y = 1;  y <= altezza; y++) {
 				
 					//il for del asse x itera il cilo che stampa ogni elemento da sinistra verso destra
-					for(int x = 0; x <= base; x++) {
+					for(int x = 1; x <= base; x++) {
 						
 					//uso un condizionale per stampare gli elementi
 						//SE y è == 0 o y == alteza cio è nel asse verticale
-						if(y == 0 || y == altezza) {
+						if(y == 1 || y == altezza) {
 							
 						//stampo il elemento da ogni iterazione in verso orizontale mentre è nella iterazione del asse x
 						System.out.print("@");	
 							
 						//ALTRIMENTI SE y == 0 o y == base 	
-						}else if(x == 0 || x == base) {
+						}else if(x == 1 || x == base) {
 							
 						//stampo il elemnto nella iterazione corrispondente  
 						System.out.print("@");
